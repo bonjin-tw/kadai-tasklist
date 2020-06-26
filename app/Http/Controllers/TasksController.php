3b30469f0ffefbed87194d5167f4ac46c51a09ce
binjin-tw <sakurasakupc@gmail.com>
2020-06-26 09:26:02 +0000

    add column

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -53,8 +53,15 @@ class TasksController extends Controller
     // postでtasks/にアクセスされた場合の「新規登録処理」
     public function store(Request $request)
     {
+        // バリデーション
+        $this->validate($request,[
+            'status' => 'required | max:10',
+            'content' => 'required | max:255',
+        ]);
+        
         // タスクを作成
         $task = new Task;
+        $task->status = $request->status;
         $task->content = $request->content;
         $task->save();
         
@@ -111,9 +118,16 @@ class TasksController extends Controller
     // putまたはpatchでtasks/(任意のid)にアクセスされた場合の「更新処理」
     public function update(Request $request, $id)
     {
+        // バリデーション
+        $this->validate($request,[
+            'status' => 'required | max:10',
+            'content' => 'required | max:255',
+        ]);
+        
         // idの値でタスクを検索して取得
         $task = Task::findOrFail($id);
         // タスクを更新
+        $task->status = $request->status;
         $task->content = $request->content;
         $task->save();
         
