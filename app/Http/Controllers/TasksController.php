b61db4bd8ae058f58939f6d0980a89a5711da7db
binjin-tw <sakurasakupc@gmail.com>
2020-07-21 11:11:40 +0000

    TaskController uppdate add_Auth

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -144,10 +144,13 @@ class TasksController extends Controller
         
         // idの値でタスクを検索して取得
         $task = Task::findOrFail($id);
-        // タスクを更新
-        $task->status = $request->status;
-        $task->content = $request->content;
-        $task->save();
+        
+        // 認証済みユーザがそのタスクの所有者である場合は、タスクを更新
+        if(\Auth::user()->id === $task->user_id){
+            $task->status = $request->status;
+            $task->content = $request->content;
+            $task->save();
+        }
         
         // トップページへリダイレクトさせる
         return redirect('/');
